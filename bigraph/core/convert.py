e11c2f9d03e02d8ec42b342f5da59a3e201f4706
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-09-23 15:17:35 +0330

    Extract node data from the networkx graph

--- bigraph/core/convert.py
+++ bigraph/core/convert.py
@@ -1,4 +1,12 @@
+from collections import defaultdict, namedtuple
 
+SingleTypeNodeIdsAndFeatures = namedtuple(
+    "SingleTypeNodeIdsAndFeatures", ["ids", "features"]
+)
+
+
+def _empty_node_info() -> SingleTypeNodeIdsAndFeatures:
+    return SingleTypeNodeIdsAndFeatures([], [])
 
 def from_networkx(
     graph,
@@ -12,3 +20,14 @@ def from_networkx(
     dtype,
 ):
     import networkx as nx
+
+    nodes = defaultdict(_empty_node_info)
+
+    features_in_node = isinstance(node_features, str)
+
+    for node_id, node_data in graph.nodes(data=True):
+        node_type = node_data.get(node_type_attr, node_type_default)
+        node_info = nodes[node_type]
+        node_info.ids.append(node_id)
+        if features_in_node:
+            node_info.features.append(node_data.get(node_features, None))
