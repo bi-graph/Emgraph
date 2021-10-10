7594f410843e1171b7115639deb9dcb1e49de5cd
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-10-10 04:57:18 +0330

    Implement load_from_csv() function

--- bigraph/datasets/datasets.py
+++ bigraph/datasets/datasets.py
@@ -1,4 +1,32 @@
+import pandas as pd
+import logging
 
 
-def load_from_csv():
-    pass
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def load_from_csv(directory_path, file_name, sep='\t', header=None, add_reciprocal_rels=False):
+
+    logger.debug('Loading data from {}.'.format(file_name))
+    df = pd.read_csv(os.path.join(directory_path, file_name),
+                     sep=sep,
+                     header=header,
+                     names=None,
+                     dtype=str)
+    logger.debug('Dropping duplicates.')
+    df = df.drop_duplicates()
+    if add_reciprocal_rels:
+        df_reciprocal = df.copy()
+
+        # swap subjects and objects
+        cols = list(df_reciprocal.columns)
+        cols[0], cols[2] = cols[2], cols[0]
+        df_reciprocal.columns = cols
+
+        # add reciprocal relations
+        df_reciprocal.iloc[:, 1] = df_reciprocal.iloc[:, 1] + "_reciprocal"
+
+        # append to original triples
+        triples_df = df.append(df_reciprocal)
+
+    return df.values
