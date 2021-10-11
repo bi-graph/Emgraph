e30974c4388a1d20c9ca49efaa9589ebce5f9d11
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-10-11 14:01:42 +0330

    _add_reciprocal_relations():
    Add and implement function

--- bigraph/datasets/datasets.py
+++ bigraph/datasets/datasets.py
@@ -16,17 +16,20 @@ def load_from_csv(directory_path, file_name, sep='\t', header=None, add_reciproc
     logger.debug('Dropping duplicates.')
     df = df.drop_duplicates()
     if add_reciprocal_rels:
-        df_reciprocal = df.copy()
-
-        # swap subjects and objects
-        cols = list(df_reciprocal.columns)
-        cols[0], cols[2] = cols[2], cols[0]
-        df_reciprocal.columns = cols
-
-        # add reciprocal relations
-        df_reciprocal.iloc[:, 1] = df_reciprocal.iloc[:, 1] + "_reciprocal"
-
-        # append to original triples
-        df = df.append(df_reciprocal)
+        df = _add_reciprocal_relations(df)
 
     return df.values
+
+
+def _add_reciprocal_relations(tri_df):
+
+    df_reciprocal = tri_df.copy()
+    # swap subjects and objects
+    cols = list(df_reciprocal.columns)
+    cols[0], cols[2] = cols[2], cols[0]
+    df_reciprocal.columns = cols
+    # add reciprocal relations
+    df_reciprocal.iloc[:, 1] = df_reciprocal.iloc[:, 1] + "_reciprocal"
+    # append to original triples
+    tri_df = tri_df.append(df_reciprocal)
+    return tri_df
