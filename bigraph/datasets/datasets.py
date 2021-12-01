1fc3fca7d7aeef24577488934ad86ea37dc7f1dc
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-12-01 19:05:35 +0330

    load_nl27k():
    1. Implement function

--- bigraph/datasets/datasets.py
+++ bigraph/datasets/datasets.py
@@ -490,3 +490,26 @@ def load_ppi5k(check_md5hash=False, clean_unseen=True, split_test_into_top_botto
         dataset = _clean_data(dataset)
 
     return generate_focusE_dataset_splits(dataset, split_test_into_top_bottom, split_threshold)
+
+
+def load_nl27k(check_md5hash=False, clean_unseen=True, split_test_into_top_bottom=True, split_threshold=0.1):
+
+    nl27k = DatasetMetadata(
+        dataset_name='nl27k',
+        filename='nl27k.zip',
+        url='https://s3-eu-west-1.amazonaws.com/ampligraph/datasets/nl27k.zip',
+        train_name='train.tsv',
+        valid_name='valid.tsv',
+        test_name='test.tsv',
+        train_checksum='d4ce775401d299074d98e046f13e7283',
+        valid_checksum='00177fa6b9f5cec18814ee599c02eae3',
+        test_checksum='2ba17f29119688d93c9d29ab40f63b3e'
+    )
+
+    dataset = _load_dataset(nl27k, data_home=None,
+                            check_md5hash=check_md5hash)
+
+    if clean_unseen:
+        dataset = _clean_data(dataset)
+
+    return generate_focusE_dataset_splits(dataset, split_test_into_top_bottom, split_threshold)
