b311f2d9e80b698960b451e0bf32fdd5674f30f3
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-12-09 20:10:38 +0330

    BigraphDatasetAdapter class:
    1. Implement use_mappings()

--- bigraph/datasets/abstract_dataset_adapter.py
+++ bigraph/datasets/abstract_dataset_adapter.py
@@ -20,3 +20,13 @@ class BigraphDatasetAdapter(abc.ABC):
         self.mapped_status = {}
         # link weights for focusE
         self.focusE_numeric_edge_values = {}
+
+
+    def use_mappings(self, rel_to_idx, ent_to_idx):
+        """Use an existing mapping with the datasource.
+        """
+        self.rel_to_idx = rel_to_idx
+        self.ent_to_idx = ent_to_idx
+        # set the mapped status to false, since we are changing the dictionary
+        for key in self.dataset.keys():
+            self.mapped_status[key] = False
