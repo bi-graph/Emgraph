458852ac1d58be8243628713d7b9b5f484a4d707
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-12-09 19:30:14 +0330

    Add abstract_dataset_adapter.py module Initialize BigraphDatasetAdapter class

---
+++ bigraph/datasets/abstract_dataset_adapter.py
@@ -0,0 +1,9 @@
+
+
+import abc
+
+class BigraphDatasetAdapter(abc.ABC):
+    """Abstract class for dataset adapters
+       Developers can use this abstract class for adapting and creating a pipeline of data to feed Bigraph.
+    """
+    pass
