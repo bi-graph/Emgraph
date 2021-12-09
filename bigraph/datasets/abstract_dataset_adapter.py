c8adf4b4f70c25a4b1f966c44c329bdedcbd262b
Soran Ghadri <soran.gdr.cs@gmail.com>
2021-12-09 19:37:28 +0330

    BigraphDatasetAdapter class:
    1. Initialize the variables

--- bigraph/datasets/abstract_dataset_adapter.py
+++ bigraph/datasets/abstract_dataset_adapter.py
@@ -6,4 +6,17 @@ class BigraphDatasetAdapter(abc.ABC):
     """Abstract class for dataset adapters
        Developers can use this abstract class for adapting and creating a pipeline of data to feed Bigraph.
     """
-    pass
+
+    def __init__(self):
+        """Initialize the class variables
+        """
+        self.dataset = {}
+
+        # relation to idx mappings
+        self.rel_to_idx = {}
+        # entities to idx mappings
+        self.ent_to_idx = {}
+        # Mapped status of each dataset
+        self.mapped_status = {}
+        # link weights for focusE
+        self.focusE_numeric_edge_values = {}
