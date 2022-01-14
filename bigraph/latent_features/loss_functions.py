ac4016b89e59ed727be0f71e4f8f9cf28bd698b5
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-01-14 09:32:24 +0330

    register_loss() function:
    + Implement
    + Add docstring

--- bigraph/latent_features/loss_functions.py
+++ bigraph/latent_features/loss_functions.py
@@ -29,3 +29,34 @@ DEFAULT_LABEL_SMOOTHING = None
 # Default label weighting for ConvE
 DEFAULT_LABEL_WEIGHTING = False
 
+def register_loss(name, external_params=[], class_params={}):
+    """
+    A wrapper for saving the wrapped loss-function in a dictionary.
+
+    :param name: name of the loss-function
+    :type name: str
+    :param external_params: A list containing the external parameters of the loss-function
+    :type external_params: list
+    :param class_params: A dictionary containing the class parameters
+    :type class_params: dict
+    :return: Class object
+    :rtype: object
+    """
+
+    default_class_params = {'require_same_size_pos_neg': True}
+
+    def populate_class_params():
+        LOSS_REGISTRY[name].class_params = {
+            'require_same_size_pos_neg': class_params.get('require_same_size_pos_neg',
+                                                          default_class_params['require_same_size_pos_neg'])
+        }
+
+    def insert_in_registry(class_handle):
+        LOSS_REGISTRY[name] = class_handle
+        class_handle.name = name
+        LOSS_REGISTRY[name].external_params = external_params
+        populate_class_params()
+        return class_handle
+
+    return insert_in_registry
+
