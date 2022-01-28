2641c8ef2f2f7ada11dda6a6033416acb7cdbc55
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-01-28 21:42:11 +0330

    max_pooling() function: 1. Implement 2. Add docstring

--- bigraph/latent_features/pool_functions.py
+++ bigraph/latent_features/pool_functions.py
@@ -29,3 +29,14 @@ def avg_pooling(embeddings):
     :rtype: tf.Operation
     """
     return tf.reduce_mean(embeddings, axis=1)
+
+def max_pooling(embeddings):
+    """
+    Max pooling of all embeddings along neighbor axis.
+
+    :param embeddings: Embedding of a list of subjects
+    :type embeddings: Tensor, shape [B, max_rel, emb_dim]
+    :return: Reduced vector v
+    :rtype: tf.Operation
+    """
+    return tf.reduce_max(embeddings, axis=1)
