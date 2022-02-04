0e57108b5de010c24ab4c22b3513c90cc7a06c61
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-02-04 20:32:39 +0330

    _create_unique_mappings() function:
    1. Implement
    2. Add docstring

--- bigraph/evaluation/protocol.py
+++ bigraph/evaluation/protocol.py
@@ -19,8 +19,19 @@ TOO_MANY_ENTITIES_TH = 50000
 
 
 def _create_unique_mappings(unique_obj, unique_rel):
+    """Create unique mappings.
+
+    :param unique_obj: Unique object
+    :type unique_obj: list
+    :param unique_rel: Unique relationship
+    :type unique_rel: list
+    :return: Rel-to-idx: Relation to idx mapping - ent-to-idx mapping: entity to idx mapping
+    :rtype: dict, dict
+    """
+
     obj_count = len(unique_obj)
     rel_count = len(unique_rel)
     rel_to_idx = dict(zip(unique_rel, range(rel_count)))
     obj_to_idx = dict(zip(unique_obj, range(obj_count)))
     return rel_to_idx, obj_to_idx
+
