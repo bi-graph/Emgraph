0393bfdf7bd950cbfcbd94a42489abfa496afdae
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-02-06 23:39:05 +0330

    get_next_batch() method:
    1. Implement
    2. Add docstring

--- bigraph/datasets/numpy_adapter.py
+++ bigraph/datasets/numpy_adapter.py
@@ -70,3 +70,50 @@ class NumpyDatasetAdapter(BigraphDatasetAdapter):
         """
 
         return dataset_type in self.dataset.keys()
+
+    def get_next_batch(self, batches_count=-1, dataset_type="train", use_filter=False):
+        """Generate the next batch of data.
+
+        :param batches_count: Number of batches per epoch (defaults to -1 means batch size of 1)
+        :type batches_count: int
+        :param dataset_type: Type of the dataset
+        :type dataset_type: str
+        :param use_filter: Whether to return the filters' metadata
+        :type use_filter: bool
+        :return: batch_output: yields a batch of triples from the dataset type specified.
+        participating_objects: all objects that were involved in the s-p-? relation. This is returned only
+            if use_filter is set to true.
+        participating_subjects: all subjects that were involved in the ?-p-o relation. This is returned only
+            if use_filter is set to true.
+        :rtype: nd-array, nd-array [n,1], nd-array [n,1]
+        """
+
+        # if data is not already mapped, then map before returning the batch
+        if not self.mapped_status[dataset_type]:
+            self.map_data()
+
+        if batches_count == -1:
+            batch_size = 1
+            batches_count = self.get_size(dataset_type)
+        else:
+            batch_size = int(np.ceil(self.get_size(dataset_type) / batches_count))
+
+        for i in range(batches_count):
+            output = []
+            out = np.int32(self.dataset[dataset_type][(i * batch_size):((i + 1) * batch_size), :])
+            output.append(out)
+
+            try:
+                focusE_numeric_edge_values_batch = self.focusE_numeric_edge_values[
+                                                       dataset_type][(i * batch_size):((i + 1) * batch_size), :]
+                output.append(focusE_numeric_edge_values_batch)
+            except KeyError:
+                pass
+
+            if use_filter:
+                # get the filter values by querying the database
+                participating_objects, participating_subjects = self.filter_adapter.get_participating_entities(out)
+                output.append(participating_objects)
+                output.append(participating_subjects)
+
+            yield output
