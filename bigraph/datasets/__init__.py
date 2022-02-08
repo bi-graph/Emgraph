938c94bf86841b660d4459983839db6f05178dca
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-02-08 20:17:20 +0330

    OneToNDatasetAdapter class:
    1. Add
    2. Implement __init__() method

--- bigraph/datasets/__init__.py
+++ bigraph/datasets/__init__.py
@@ -5,4 +5,11 @@ from .datasets import load_from_csv, load_from_rdf, load_fb15k, load_wn18, load_
 
 from .abstract_dataset_adapter import BigraphDatasetAdapter
 from .sqlite_adapter import SQLiteAdapter
+from .numpy_adapter import NumpyDatasetAdapter
+from .oneton_adapter import OneToNDatasetAdapter
+
+__all__ = ['load_from_csv', 'load_from_rdf', 'load_from_ntriples', 'load_wn18', 'load_fb15k',
+           'load_fb15k_237', 'load_yago3_10', 'load_wn18rr', 'load_wn11', 'load_fb13',
+           'load_onet20k', 'load_ppi5k', 'load_nl27k', 'load_cn15k',
+           'BigraphDatasetAdapter', 'NumpyDatasetAdapter', 'SQLiteAdapter', 'OneToNDatasetAdapter']
 
