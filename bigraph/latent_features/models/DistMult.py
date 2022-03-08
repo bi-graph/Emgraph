e52f9afef9e164ca96eca1be5c4fcb83dace9404
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-03-08 23:08:20 +0330

    Add DistMult.py module

---
+++ bigraph/latent_features/models/DistMult.py
@@ -0,0 +1,7 @@
+
+from .EmbeddingModel import EmbeddingModel, register_model
+from bigraph.latent_features import constants as constants
+from bigraph.latent_features.initializers import DEFAULT_XAVIER_IS_UNIFORM
+import tensorflow as tf
+
+
