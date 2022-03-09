6928c420d7e2682f7dec35aec138911af8430d0e
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-03-09 21:48:55 +0330

    DistMult class:
    1. Implement __init__() method
    2. Add details to docstrings

--- bigraph/latent_features/models/DistMult.py
+++ bigraph/latent_features/models/DistMult.py
@@ -5,3 +5,163 @@ from bigraph.latent_features.initializers import DEFAULT_XAVIER_IS_UNIFORM
 import tensorflow as tf
 
 
+@register_model("DistMult",
+                ["normalize_ent_emb", "negative_corruption_entities"])
+class DistMult(EmbeddingModel):
+    r"""The DistMult model :cite:`yang2014embedding`.
+
+
+    The bilinear diagonal DistMult model uses the trilinear dot product as scoring function:
+
+    .. math::
+
+        f_{DistMult}=\langle \mathbf{r}_p, \mathbf{e}_s, \mathbf{e}_o \rangle
+
+    where :math:`\mathbf{e}_{s}` is the embedding of the subject, :math:`\mathbf{r}_{p}` the embedding
+    of the predicate and :math:`\mathbf{e}_{o}` the embedding of the object.
+
+    Examples:
+
+    >>> import numpy as np
+    >>> from bigraph.latent_features import DistMult
+    >>> model = DistMult(batches_count=1, seed=555, epochs=20, k=10, loss='pairwise',
+    >>>         loss_params={'margin':5})
+    >>> X = np.array([['a', 'y', 'b'],
+    >>>               ['b', 'y', 'a'],
+    >>>               ['a', 'y', 'c'],
+    >>>               ['c', 'y', 'a'],
+    >>>               ['a', 'y', 'd'],
+    >>>               ['c', 'y', 'd'],
+    >>>               ['b', 'y', 'c'],
+    >>>               ['f', 'y', 'e']])
+    >>> model.fit(X)
+    >>> model.predict(np.array([['f', 'y', 'e'], ['b', 'y', 'd']]))
+    [-0.13863425, -0.09917116]
+    >>> model.get_embeddings(['f','e'], embedding_type='entity')
+    array([[ 0.10137264, -0.28248304,  0.6153027 , -0.13133956, -0.11675504,
+    -0.37876177,  0.06027773, -0.26390398,  0.254603  ,  0.1888549 ],
+    [-0.6467299 , -0.13729756,  0.3074872 ,  0.16966867, -0.04098966,
+     0.25289047, -0.2212451 , -0.6527815 ,  0.5657673 , -0.03876532]],
+    dtype=float32)
+
+    """
+
+    def __init__(self,
+                 k=constants.DEFAULT_EMBEDDING_SIZE,
+                 eta=constants.DEFAULT_ETA,
+                 epochs=constants.DEFAULT_EPOCH,
+                 batches_count=constants.DEFAULT_BATCH_COUNT,
+                 seed=constants.DEFAULT_SEED,
+                 embedding_model_params={'normalize_ent_emb': constants.DEFAULT_NORMALIZE_EMBEDDINGS,
+                                         'negative_corruption_entities': constants.DEFAULT_CORRUPTION_ENTITIES,
+                                         'corrupt_sides': constants.DEFAULT_CORRUPT_SIDE_TRAIN},
+                 optimizer=constants.DEFAULT_OPTIM,
+                 optimizer_params={'lr': constants.DEFAULT_LR},
+                 loss=constants.DEFAULT_LOSS,
+                 loss_params={},
+                 regularizer=constants.DEFAULT_REGULARIZER,
+                 regularizer_params={},
+                 initializer=constants.DEFAULT_INITIALIZER,
+                 initializer_params={'uniform': DEFAULT_XAVIER_IS_UNIFORM},
+                 verbose=constants.DEFAULT_VERBOSE):
+        """Initialize an DistMult
+
+        Also creates a new Tensorflow session for training.
+
+        :param k: Embedding space dimensionality
+        :type k: int
+        :param eta: The number of negatives that must be generated at runtime during training for each positive.
+        :type eta: int
+        :param epochs: The iterations of the training loop.
+        :type epochs: int
+        :param batches_count: The number of batches in which the training set must be split during the training loop.
+        :type batches_count: int
+        :param seed:The seed used by the internal random numbers generator.
+        :type seed: int
+        :param embedding_model_params: DistMult-specific hyperparams, passed to the model as a dictionary.
+
+            Supported keys:
+
+            - **'normalize_ent_emb'** (bool): flag to indicate whether to normalize entity embeddings
+              after each batch update (default: False).
+            - **'negative_corruption_entities'** - Entities to be used for generation of corruptions while training.
+              It can take the following values :
+              ``all`` (default: all entities),
+              ``batch`` (entities present in each batch),
+              list of entities
+              or an int (which indicates how many entities that should be used for corruption generation).
+            - **corrupt_sides** : Specifies how to generate corruptions for training.
+              Takes values `s`, `o`, `s+o` or any combination passed as a list
+            - **'non_linearity'**: can be one of the following values ``linear``, ``softplus``, ``sigmoid``, ``tanh``
+            - **'stop_epoch'**: specifies how long to decay (linearly) the numeric values from 1 to original value
+            until it reachs original value.
+            - **'structural_wt'**: structural influence hyperparameter [0, 1] that modulates the influence of graph
+            topology.
+            - **'normalize_numeric_values'**: normalize the numeric values, such that they are scaled between [0, 1]
+
+            The last 4 parameters are related to FocusE layers.
+
+            Example: ``embedding_model_params={'normalize_ent_emb': False}``
+        :type embedding_model_params: dict
+        :param optimizer: The optimizer used to minimize the loss function. Choose between 'sgd',
+            'adagrad', 'adam', 'momentum'.
+        :type optimizer: str
+        :param optimizer_params: Arguments specific to the optimizer, passed as a dictionary.
+
+            Supported keys:
+
+            - **'lr'** (float): learning rate (used by all the optimizers). Default: 0.1.
+            - **'momentum'** (float): learning momentum (only used when ``optimizer=momentum``). Default: 0.9.
+
+            Example: ``optimizer_params={'lr': 0.01}``
+        :type optimizer_params: dict
+        :param loss: The type of loss function to use during training.
+
+            - ``pairwise``  the model will use pairwise margin-based loss function.
+            - ``nll`` the model will use negative loss likelihood.
+            - ``absolute_margin`` the model will use absolute margin likelihood.
+            - ``self_adversarial`` the model will use adversarial sampling loss function.
+            - ``multiclass_nll`` the model will use multiclass nll loss.
+              Switch to multiclass loss defined in :cite:`chen2015` by passing 'corrupt_sides'
+              as ['s','o'] to embedding_model_params.
+              To use loss defined in :cite:`kadlecBK17` pass 'corrupt_sides' as 'o' to embedding_model_params.
+        :type loss: str
+        :param loss_params: Dictionary of loss-specific hyperparameters. See :ref:`loss functions <loss>`
+            documentation for additional details.
+
+            Example: ``optimizer_params={'lr': 0.01}`` if ``loss='pairwise'``.
+        :type loss_params: dict
+        :param regularizer: The regularization strategy to use with the loss function.
+
+            - ``None``: the model will not use any regularizer (default)
+            - 'LP': the model will use L1, L2 or L3 based on the value of ``regularizer_params['p']`` (see below).
+        :type regularizer: str
+        :param regularizer_params: Dictionary of regularizer-specific hyperparameters. See the :ref:`regularizers <ref-reg>`
+            documentation for additional details.
+
+            Example: ``regularizer_params={'lambda': 1e-5, 'p': 2}`` if ``regularizer='LP'``.
+        :type regularizer_params: dict
+        :param initializer: The type of initializer to use.
+
+            - ``normal``: The embeddings will be initialized from a normal distribution
+            - ``uniform``: The embeddings will be initialized from a uniform distribution
+            - ``xavier``: The embeddings will be initialized using xavier strategy (default)
+        :type initializer: str
+        :param initializer_params: Dictionary of initializer-specific hyperparameters. See the
+            :ref:`initializer <ref-init>`
+            documentation for additional details.
+
+            Example: ``initializer_params={'mean': 0, 'std': 0.001}`` if ``initializer='normal'``.
+        :type initializer_params: dict
+        :param verbose: Verbose mode
+        :type verbose: bool
+        """
+
+        super().__init__(k=k, eta=eta, epochs=epochs, batches_count=batches_count, seed=seed,
+                         embedding_model_params=embedding_model_params,
+                         optimizer=optimizer, optimizer_params=optimizer_params,
+                         loss=loss, loss_params=loss_params,
+                         regularizer=regularizer, regularizer_params=regularizer_params,
+                         initializer=initializer, initializer_params=initializer_params,
+                         verbose=verbose)
+
