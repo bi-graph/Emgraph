f65f5c705ff6fd36cc914400235a2f2ce2fad8f2
Soran Ghadri <soran.gdr.cs@gmail.com>
2022-03-10 21:56:30 +0330

    1. Implement fit() method 2. Add details to the docstring

--- bigraph/latent_features/models/RandomBaseline.py
+++ bigraph/latent_features/models/RandomBaseline.py
@@ -71,3 +71,77 @@ class RandomBaseline(EmbeddingModel):
             return tf.random_uniform((tf.size(e_s),), minval=0, maxval=1) + useless
         else:
             return tf.random_uniform((tf.size(e_s),), minval=0, maxval=1)
+
+    def fit(self, X, early_stopping=False, early_stopping_params={}, focusE_numeric_edge_values=None,
+            tensorboard_logs_path=None):
+        """Train an EmbeddingModel (with optional early stopping).
+        There is no actual training involved in practice and the early stopping parameters won't have any effect.
+
+        :param X: Numpy array of training triples OR handle of Dataset adapter which would help retrieve data.
+        :type X: ndarray (shape [n, 3]) or object of BigraphDatasetAdapter
+        :param early_stopping: Flag to enable early stopping (default:False).
+
+        If set to ``True``, the training loop adopts the following early stopping heuristic:
+
+        - The model will be trained regardless of early stopping for ``burn_in`` epochs.
+        - Every ``check_interval`` epochs the method will compute the metric specified in ``criteria``.
+
+        If such metric decreases for ``stop_interval`` checks, we stop training early.
+
+        Note the metric is computed on ``x_valid``. This is usually a validation set that you held out.
+
+        Also, because ``criteria`` is a ranking metric, it requires generating negatives.
+        Entities used to generate corruptions can be specified, as long as the side(s) of a triple to corrupt.
+        The method supports filtered metrics, by passing an array of positives to ``x_filter``. This will be used to
+        filter the negatives generated on the fly (i.e. the corruptions).
+
+        .. note::
+
+            Keep in mind the early stopping criteria may introduce a certain overhead
+            (caused by the metric computation).
+            The goal is to strike a good trade-off between such overhead and saving training epochs.
+
+            A common approach is to use MRR unfiltered: ::
+
+                early_stopping_params={x_valid=X['valid'], 'criteria': 'mrr'}
+
+            Note the size of validation set also contributes to such overhead.
+            In most cases a smaller validation set would be enough.
+        :type early_stopping: bool
+        :param early_stopping_params: Dictionary of hyperparameters for the early stopping heuristics.
+
+        The following string keys are supported:
+
+            - **'x_valid'**: ndarray, shape [n, 3] : Validation set to be used for early stopping.
+            - **'criteria'**: string : criteria for early stopping 'hits10', 'hits3', 'hits1' or 'mrr'(default).
+            - **'x_filter'**: ndarray, shape [n, 3] : Positive triples to use as filter if a 'filtered'
+              early stopping criteria is desired (i.e. filtered-MRR if 'criteria':'mrr').
+              Note this will affect training time (no filter by default).
+            - **'burn_in'**: int : Number of epochs to pass before kicking in early stopping (default: 100).
+            - **check_interval'**: int : Early stopping interval after burn-in (default:10).
+            - **'stop_interval'**: int : Stop if criteria is performing worse over n consecutive checks (default: 3)
+            - **'corruption_entities'**: List of entities to be used for corruptions.
+              If 'all', it uses all entities (default: 'all')
+            - **'corrupt_side'**: Specifies which side to corrupt. 's', 'o', 's+o' (default)
+
+            Example: ``early_stopping_params={x_valid=X['valid'], 'criteria': 'mrr'}``
+        :type early_stopping_params: dict
+        :param focusE_numeric_edge_values: Numeric values associated with links. Semantically, the numeric value can
+        signify importance, uncertainity, significance, confidence, etc. If the numeric value is unknown pass a NaN
+        weight. The model will uniformly randomly assign a numeric value. One can also think about assigning numeric
+        values by looking at the distribution of it per predicate.
+        :type focusE_numeric_edge_values: nd array (n, 1)
+        :param tensorboard_logs_path: Path to store tensorboard logs, e.g. average training loss tracking per epoch
+        (default: ``None`` indicating no logs will be collected). When provided it will create a folder under provided
+        path and save tensorboard files there. To then view the loss in the terminal run: ``tensorboard --logdir
+        <tensorboard_logs_path>``.
+        :type tensorboard_logs_path: str or None
+        Path to store tensorboard logs, e.g. average training loss tracking per epoch (default: ``None`` indicating
+        no logs will be collected). When provided it will create a folder under provided path and save tensorboard
+        files there. To then view the loss in the terminal run: ``tensorboard --logdir <tensorboard_logs_path>``.
+        :return:
+        :rtype:
+        """
+
+        super().fit(X, early_stopping, early_stopping_params, focusE_numeric_edge_values,
+                    tensorboard_logs_path=tensorboard_logs_path)
