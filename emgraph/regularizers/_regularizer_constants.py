110f3a022ca2ce6819865c5807d6180b7452d10d
sigma1326 <taleb.zarhesh@gmail.com>
2022-07-30 22:34:12 +0430

    + Update Codes

--- emgraph/regularizers/_regularizer_constants.py
+++ emgraph/regularizers/_regularizer_constants.py
@@ -4,7 +4,7 @@ REGULARIZER_REGISTRY = {}
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# defalut lambda to be used in L1, L2 and L3 regularizer
+# default lambda to be used in L1, L2 and L3 regularizer
 DEFAULT_LAMBDA = 1e-5
 # default regularization - L2
 DEFAULT_NORM = 2
