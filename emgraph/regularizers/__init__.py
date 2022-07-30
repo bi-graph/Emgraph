77a116b91a3a111eee382c094439447e8185a943
sigma1326 <taleb.zarhesh@gmail.com>
2022-07-30 22:33:45 +0430

    + Update Codes

--- emgraph/regularizers/__init__.py
+++ emgraph/regularizers/__init__.py
@@ -5,4 +5,8 @@ from ._regularizer_constants import REGULARIZER_REGISTRY
 from .lp import LPRegularizer
 from .regularizer import Regularizer
 
-__all__ = ["LPRegularizer", "Regularizer", "REGULARIZER_REGISTRY"]
+__all__ = [
+    "LPRegularizer",
+    "Regularizer",
+    "REGULARIZER_REGISTRY",
+]
